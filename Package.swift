e9f17824e0606f6479816d99ffdec17b18d54150
Peter Wilkniss <peter@clevertap.com>
2022-03-06 15:15:06 -0800

    fix package

--- Package.swift
+++ Package.swift
@@ -10,21 +10,14 @@ let package = Package(
     products: [
         .library(
             name: "DirectCallSDK",
-            targets: ["DirectCall"]
+            targets: ["DirectCallSDK"]
         )
     ],
     targets: [
-        .target(
-            name: "DirectCall",
-            dependencies: ["SocketIO", "DirectCallSDK"]
-        ),
         .binaryTarget(
             name: "DirectCallSDK",
             url: "https://github.com/CleverTap/clevertap-directcall-ios-sdk/releases/download/0.0.1-alpha/DirectCallSDK.xcframework.zip",
             checksum: "9e8b6e5df5812544d76b3398be0df4eabfbb5ea2d6abfefb8e50a85d06e0d3e4"
         )
-    ],
-    dependencies: [
-        .package(url: "https://github.com/socketio/socket.io-client-swift.git", from: "15.2.0")
     ]
 )
